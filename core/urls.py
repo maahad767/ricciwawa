c4e9bdf1bffd4b9c1d1ad6372fc9c20537a3bd6d
Mohammad Abdul Ahad <maahad.767@gmail.com>
2021-10-03 18:37:05 +0600

    Bugs fixed

--- core/urls.py
+++ core/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls', namespace='account')),
-    path('utilss/', include('utils.urls', namespace='utils')),
+    path('utils/', include('utils.urls', namespace='utils')),
 ]
