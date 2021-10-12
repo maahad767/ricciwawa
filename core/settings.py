45f435a6e4da4157ca0ba8b9c995ae8797320a92
Mohammad Abdul Ahad <maahad.767@gmail.com>
2021-10-12 15:25:38 +0600

    allowed host

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-+(qgy3*el_%em1s*r+!-ao95hyl!5mqe0g=5cj^3)@(w_2vw0v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
