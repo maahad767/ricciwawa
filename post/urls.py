dd50d7078215129a2f46db1d67cd4bc5aa51bfc9
Mohammad Abdul Ahad <maahad.767@gmail.com>
2021-10-14 14:25:10 +0600

    Added URL endpoints for some APIs.

--- post/urls.py
+++ post/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from rest_framework import routers
 
 from post.views import SubscriptionViewset, PlaylistViewset, PostViewset, CommentViewset, NewsfeedView, \
-    FavouriteVocabularyViewset, LikePostView
+    FavouriteVocabularyViewset, LikePostView, UnlikePostView, ViewPostView, FollowView, AddFavouriteView, UnfollowView, \
+    RemoveFavouriteView, SavePlaylistView, UnsavePlaylistView, SubscribeView
 
 """
 Router is used to route ViewSets. 
@@ -20,8 +21,16 @@ router.register(r'comment', CommentViewset, basename='post')
 router.register(r'fav-vocab', FavouriteVocabularyViewset, basename='post')
 
 urlpatterns = [
-    path('get-newsfeed-contents', NewsfeedView.as_view()),
-    path('like-post/<int:id>', LikePostView.as_view()),
-
-
+    path('get-newsfeed-contents/', NewsfeedView.as_view()),
+    path('like-post/', LikePostView.as_view()),
+    path('unlike-post/', UnlikePostView.as_view()),
+    path('mark-post-viewd/', ViewPostView.as_view()),
+    path('follow-user/', FollowView.as_view()),
+    path('unfollow-user/', UnfollowView.as_view()),
+    path('add-to-favrourite-post/', AddFavouriteView.as_view()),
+    path('remove-from-favrourite-post/', RemoveFavouriteView.as_view()),
+    path('save-playlist/', SavePlaylistView.as_view()),
+    path('unsave-playlist/', UnsavePlaylistView.as_view()),
+    path('subscribe-plan/', SubscribeView.as_view()),
+    path('unsubscribe-plan/', SubscribeView.as_view()),
 ] + router.urls
