402087f2ed542de36cc2178d13fc08cf6e0cd97c
Mohammad Abdul Ahad <maahad.767@gmail.com>
2022-01-11 00:03:01 +0600

    bug fixes

--- post/signals.py
+++ post/signals.py
@@ -25,7 +25,6 @@ def add_audio_in_post(instance, created, *args, **kwargs):
     if instance.attachment:
         ext = instance.attachment.split('.')[-1]
         instance.attachment = "attachment_" + str_hashed_id + "." + ext  # change file name
-        instance.save()
 
     if instance.text_simplified_chinese:
         sim_spaced_sentence = "\n".join(instance.text_simplified_chinese)
@@ -33,7 +32,6 @@ def add_audio_in_post(instance, created, *args, **kwargs):
         sim_spaced_sentence = sim_spaced_sentence.replace("<p>", "\n").replace("<BR>", "\n<BR>\n")
         instance.audio_simplified_chinese = storage_prefix + str_hashed_id + "_tw" + ".mp3"
         instance.timing_simplified_chinese = storage_prefix + str_hashed_id + "_tw" + "_timing.txt"
-        instance.save()
         create_mp3_task.delay("tw", sim_spaced_sentence, instance.audio_simplified_chinese)
 
     if instance.text_traditional_chinese:
@@ -42,11 +40,11 @@ def add_audio_in_post(instance, created, *args, **kwargs):
         trad_spaced_sentence = trad_spaced_sentence.replace("<p>", "\n").replace("<BR>", "\n<BR>\n")
         instance.audio_traditional_chinese = storage_prefix + str_hashed_id + "_hk" + ".mp3"
         instance.timing_traditional_chinese = storage_prefix + str_hashed_id + "_hk" + "_timing.txt"
-        instance.save()
         create_mp3_task.delay("hk", trad_spaced_sentence, instance.audio_traditional_chinese)
 
+    instance.save()
     if instance.text_traditional_chinese and instance.text_simplified_chinese and instance.meaning_words and instance.pin_yin_words:
-        add_full_data_translations.delay(instance.id, instance.text_traditional_chinese, instance.text_simplified_chinese, instance.meaning_words, instance.pin_yin_words)
+        add_full_data_translations.delay(instance.id)
 
 
 """
