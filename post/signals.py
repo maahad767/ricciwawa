686068a6ba0437368a17238bcc3c23f63d4ce16d
Mohammad Abdul Ahad <maahad.767@gmail.com>
2022-01-11 22:42:29 +0600

    rolled back

--- post/signals.py
+++ post/signals.py
@@ -46,6 +46,7 @@ def add_audio_in_post(instance, created, *args, **kwargs):
     if instance.text_traditional_chinese and instance.text_simplified_chinese and instance.meaning_words and instance.pin_yin_words:
         add_full_data_translations.delay(instance.id)
 
+
 """
 TYPES = (
     (0, 'announcement'),
