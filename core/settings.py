522be1333df70c9c687535fe860c2c7570e0c1ab
Mohammad Abdul Ahad <maahad.767@gmail.com>
2022-01-23 15:37:17 +0600

    cloud sql test

--- core/settings.py
+++ core/settings.py
@@ -91,15 +91,26 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'ricciwawadb',
+#         'USER': 'ricciwawadbuser',
+#         'PASSWORD': 'abcdefgh123',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ricciwawadb',
-        'USER': 'ricciwawadbuser',
-        'PASSWORD': 'abcdefgh123',
+        'NAME': 'riccidb',
+        'USER': 'ricciuser',
+        'PASSWORD': 'test123456',
         'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'PORT': '5555',
     }
 }
 
